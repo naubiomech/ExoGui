f4e8c6fba0b1b58990a8db04ece06d83dc7e0c0f
Jack Austin Garrard <jg2562@nau.edu>
2019-02-09 20:14:29 -0700

    Added a empty message check and variable for debugging

--- get_message.m
+++ get_message.m
@@ -3,12 +3,18 @@ function [msg, data] = get_message()
     global GUI_Variables
     bt = GUI_Variables.BT;
     message = fgetl(bt);
+    if length(message) == 0
+        msg = '\0';
+        data = [];
+        return;
+    end
     msg = message(2);
     if message(1) == 83 && message(length(message)-1) == 90
         indexes = find(message==',');
         data=zeros(1,length(indexes)-1);
         for index_iterator = 1:(length(indexes)-1)
-            data(index_iterator) = str2double(message((indexes(index_iterator)+1):(indexes(index_iterator+1)-1)));
+            raw_data = message((indexes(index_iterator)+1):(indexes(index_iterator+1)-1));
+            data(index_iterator) = str2double(raw_data);
         end
         data = data ./ 100;
     end
