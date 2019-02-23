1975c5e0f8257ca6bce7274e897237be2b751ee0
Jack Garrard <jg2562@nau.edu>
2019-02-22 20:13:01 -0700

    Moved getting status data into command

--- command.m
+++ command.m
@@ -1,10 +1,50 @@
-function [] = command(msg, data, handles)
-global GUI_Variables;
+function GUI_Variables = command(GUI_Variables, msg, data, handles)
 %This function Figures out what non-data
 %message the bluetooth sent
 
 % i.e. it is used when matlab receive from arduino a message non-data
 switch(msg)
+  case '?'% It means it is data message to plot and update signals
+    RLCount = GUI_Variables.RLCount;
+    LLCount = GUI_Variables.LLCount;
+    
+    GUI_Variables.RLTRQ(RLCount) = data(1);                 %Gets the new Torque Value and Stores it
+    GUI_Variables.RLFSR(RLCount) = data(2); %state
+    GUI_Variables.RLSET(RLCount) = data(3); %save the torque set point
+    GUI_Variables.RLVOLT(RLCount) = data(4);
+    GUI_Variables.RLVOLT_H(RLCount) = data(5);
+    GUI_Variables.SIG1(RLCount) = data(11);
+    GUI_Variables.SIG3(RLCount) = data(13);
+    GUI_Variables.SIG4(RLCount) = data(14);%data(14)/100;
+    GUI_Variables.BASER(RLCount)=GUI_Variables.baser;
+
+    GUI_Variables.R_BAL_DYN_HEEL(RLCount)=GUI_Variables.R_Bal_dyn_Heel;
+    GUI_Variables.R_BAL_STEADY_HEEL(RLCount)=GUI_Variables.R_Bal_steady_Heel;
+    GUI_Variables.R_BAL_DYN_TOE(RLCount)=GUI_Variables.R_Bal_dyn_Toe;
+    GUI_Variables.R_BAL_STEADY_TOE(RLCount)=GUI_Variables.R_Bal_steady_Toe;
+    GUI_Variables.BASEL_BIOFB(RLCount)=GUI_Variables.basel_biofb;
+           
+    GUI_Variables.LLTRQ(LLCount) = data(6);            %Gets the new Torque Value and stores it
+    GUI_Variables.LLFSR(LLCount) = data(7);
+    GUI_Variables.LLSET(LLCount) = data(8); %New to save also the set point
+    GUI_Variables.LLVOLT(LLCount) = data(9);
+    GUI_Variables.LLVOLT_H(LLCount) = data(10);
+    GUI_Variables.SIG2(LLCount) = data(12);
+
+    GUI_Variables.BASEL(LLCount)=GUI_Variables.basel;
+
+    GUI_Variables.L_BAL_DYN_HEEL(LLCount)=GUI_Variables.L_Bal_dyn_Heel;
+    GUI_Variables.L_BAL_STEADY_HEEL(LLCount)=GUI_Variables.L_Bal_steady_Heel;
+    GUI_Variables.L_BAL_DYN_TOE(LLCount)=GUI_Variables.L_Bal_dyn_Toe;
+    GUI_Variables.L_BAL_STEADY_TOE(LLCount)=GUI_Variables.L_Bal_steady_Toe;
+
+    GUI_Variables.LLCount = LLCount + 1;
+    GUI_Variables.RLCount = RLCount + 1;
+    if(data(2)==9)||(data(7)==9)
+        disp("Torque value problem    Trq > 25Nm");
+        set(handles.statusText,'String','Problem Trq Ctrl, Trq > 25 Nm');
+
+    end
   case '`'
     KF_LL = data(1);                                          %Gets the Current Arduino Torque Setpoint
     set(handles.L_Check_KF_Text,'String',KF_LL);
