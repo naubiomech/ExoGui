afe4755eb55330183f6332938fc6b500100fa239
Thang <thangnthn@gmail.com>
2019-04-26 19:36:00 -0700

    Combined the save parameters for the PC with the end trial button

--- A_EXO_s.m
+++ A_EXO_s.m
@@ -54,7 +54,7 @@ function A_EXO_s_OpeningFcn(hObject, ~, handles, varargin)
 
     handles.output = hObject;
 
-    BT_INDEX = 3;
+    BT_INDEX = 4;
     BT_NAMES={'Exo_Bluetooth_3','Capstone_Bluetooth_1', ...
               'Exo_Bluetooth_2','Exo_High_Power','Jacks_Bluetooth', 'Jasons_Bluetooth'};
     BT_NAME = BT_NAMES{BT_INDEX};
@@ -881,7 +881,69 @@ function End_Trial_Callback(hObject, eventdata, handles)
         fprintf(fileID,['KD_R = ',num2str(rkd),'\n']);
         fprintf(fileID,['KI_R = ',num2str(rki),'\n']);
         fclose(fileID);
+        
+%  TN 04-26-2019        
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
+        left_plant_peak_mean = 0; 
+        right_plant_peak_mean = 0; 
+        left_leg_Curr_Combined = 0;
+        right_leg_Curr_Combined = 0;
+        left_leg_fsr_Combined_peak_ref = 0;
+        right_leg_fsr_Combined_peak_ref = 0;
+        left_leg_fsr_Toe_peak_ref = 0;
+        right_leg_fsr_Toe_peak_ref = 0;
+        left_leg_fsr_Heel_peak_ref = 0;
+        right_leg_fsr_Heel_peak_ref = 0;
+        left_leg_torque_calibration_value = 0;
+        right_leg_torque_calibration_value = 0;
 
+
+    fwrite(bt,'e');
+          message = fgetl(bt);
+        if message(1) == 83 && message(length(message)-1) == 90 && message(2) == 'P'
+            indexes = find(message==44);
+        left_plant_peak_mean = str2double(message((indexes(1)+1):(indexes(2)-1)));
+        right_plant_peak_mean = str2double(message((indexes(2)+1):(indexes(3)-1)));
+        left_leg_Curr_Combined = str2double(message((indexes(3)+1):(indexes(4)-1)));
+        right_leg_Curr_Combined = str2double(message((indexes(4)+1):(indexes(5)-1)));
+        left_leg_fsr_Combined_peak_ref = str2double(message((indexes(5)+1):(indexes(6)-1)));
+        right_leg_fsr_Combined_peak_ref = str2double(message((indexes(6)+1):(indexes(7)-1)));
+        left_leg_fsr_Toe_peak_ref = str2double(message((indexes(7)+1):(indexes(8)-1)));
+        right_leg_fsr_Toe_peak_ref = str2double(message((indexes(8)+1):(indexes(9)-1)));
+        left_leg_fsr_Heel_peak_ref = str2double(message((indexes(9)+1):(indexes(10)-1)));
+        right_leg_fsr_Heel_peak_ref = str2double(message((indexes(10)+1):(indexes(11)-1)));
+        left_leg_torque_calibration_value = str2double(message((indexes(11)+1):(indexes(12)-1)));
+        right_leg_torque_calibration_value = str2double(message((indexes(12)+1):(indexes(13)-1)));
+
+        
+        end
+            
+ %   currDir = cd;       % Current directory
+        saveDir_P = [GUI_Variables.SSID,'_',date,'_Proportional_Parameters'];    % Save directory specific to subject and date
+            savePath_P = [currDir,'\',saveDir_P];    % Save directory specific to subject and date
+        if ~exist(saveDir_P, 'dir')
+            mkdir(currDir, saveDir_P);           % Make a save directory
+        end
+        Filename_P = sprintf('%s_%d.txt',fullfile(savePath_P,[GUI_Variables.SSID,'_',date,'_',...
+            GUI_Variables.TimeStamp,'_','Proportional_Parameters_']),bt.UserData);               %Creates a new filename called "Torque_#"
+                                                                           %Where # is the trial number                                                                           
+        fileID_P = fopen(Filename_P,'w');                                      %Actually creates that file
+        pause(.01);
+        fprintf(fileID_P,['left_plant_peak_mean = ', num2str(left_plant_peak_mean),'\r\n']);
+        fprintf(fileID_P,['right_plant_peak_mean = ', num2str(right_plant_peak_mean),'\r\n']);
+        fprintf(fileID_P,['left_leg_Curr_Combined = ', num2str(left_leg_Curr_Combined),'\r\n']);
+        fprintf(fileID_P,['right_leg_Curr_Combined = ', num2str(right_leg_Curr_Combined),'\r\n']);
+        fprintf(fileID_P,['left_leg_fsr_Combined_peak_ref = ', num2str(left_leg_fsr_Combined_peak_ref),'\r\n']);
+        fprintf(fileID_P,['right_leg_fsr_Combined_peak_ref = ', num2str(right_leg_fsr_Combined_peak_ref),'\r\n']);
+        fprintf(fileID_P,['left_leg_fsr_Toe_peak_ref = ', num2str(left_leg_fsr_Toe_peak_ref),'\r\n']);
+        fprintf(fileID_P,['right_leg_fsr_Toe_peak_ref = ', num2str(right_leg_fsr_Toe_peak_ref),'\r\n']);
+        fprintf(fileID_P,['left_leg_fsr_Heel_peak_ref = ', num2str(left_leg_fsr_Heel_peak_ref),'\r\n']);
+        fprintf(fileID_P,['right_leg_fsr_Heel_peak_ref = ', num2str(right_leg_fsr_Heel_peak_ref),'\r\n']);
+        fprintf(fileID_P,['left_leg_torque_calibration_value = ', num2str(left_leg_torque_calibration_value),'\r\n']);
+        fprintf(fileID_P,['right_leg_torque_calibration_value = ', num2str(right_leg_torque_calibration_value),'\r\n']);
+        fclose(fileID_P);
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
         set(handles.statusText,'String','Data has finished being Saved');
         GUI_Variables = Reset_GUI_Variables(GUI_Variables);
         bt.UserData = bt.UserData + 1; % Increments the trial number
