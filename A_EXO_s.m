d930fa0c66f4746c0063c8757c1f92ddae15fc62
Greg Orekhov <go226@nau.edu>
2020-01-15 16:11:58 -0700

    Added radio buttons for different motor assemblies when in current control or current diagnostic mode

--- A_EXO_s.m
+++ A_EXO_s.m
@@ -22,7 +22,7 @@ function varargout = A_EXO_s(varargin)
 
 % Edit the above text to modify the response to help A_EXO_s
 
-% Last Modified by GUIDE v2.5 04-Dec-2019 16:59:16
+% Last Modified by GUIDE v2.5 15-Jan-2020 13:23:43
 
 % Begin initialization code - DO NOT EDIT
     gui_Singleton = 1;
@@ -86,8 +86,9 @@ function A_EXO_s_OpeningFcn(hObject, ~, handles, varargin)
                            'L_BAL_DYN_TOE',20*ones(1,60000),'L_BAL_DYN_HEEL',30*ones(1,60000),'L_BAL_STEADY_TOE',40*ones(1,60000),'L_BAL_STEADY_HEEL',50*ones(1,60000),...
                            'R_BAL_DYN_TOE',20*ones(1,60000),'R_BAL_DYN_HEEL',30*ones(1,60000),'R_BAL_STEADY_TOE',40*ones(1,60000),'R_BAL_STEADY_HEEL',50*ones(1,60000),...
                            'PropOn',0,'SSID','No_ID','TimeStamp',' ','ReuseBaseline',1,'LapBaseline',0,...
-                           'PtbOn',0,'old_TM_right',0,'TM_right',0,'old_TM_left',0,'TM_left',0,'flag_TM_left',0,'flag_TM_right',0,'TM_counter',1);
+                           'PtbOn',0,'old_TM_right',0,'TM_right',0,'old_TM_left',0,'TM_left',0,'flag_TM_left',0,'flag_TM_right',0,'TM_counter',1);  
     
+    set(findall(handles.MotorParamSelect,'-property','value'),'value',0);
     if get(handles.Activate_Prop_Pivot,'Value') %GO 5/7/19 - GUIDE is not cooperating so brute force
         set(handles.Activate_Prop_Pivot,'Value',0); 
     end
@@ -4745,15 +4746,64 @@ end
 
 
 % --- Executes on button press in OLCurrentControl.
-function OLCurrentControl_Callback(~, ~, handles)
+function OLCurrentControl_Callback(hObject, ~, handles)
 % hObject    handle to OLCurrentControl (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)% function [rkp,rkd,rki]=R_Get_PID_Callback(~, ~, handles)
+% handles    structure with handles and user data (see GUIDATA)
 % SEND 'k' char 107
-    GUI_Variables = handles.GUI_Variables;
-    bt = GUI_Variables.BT;
 
-    if(bt.Status=="open")
-        fwrite(bt,char(107));
-    end
+GUI_Variables = handles.GUI_Variables;
+bt = GUI_Variables.BT;
+    
+if get(hObject,'Value')
+    set(handles.L_Get_Setpoint,'String','Get Current');
+    set(handles.L_Set_Setpoint,'String','Set Current');
+    set(handles.Torque_Panel,'Title','Open-Loop Current Control');
+    set(handles.CurrentDiagnosticsCheckbox,'Enable','off');
+    set(handles.statusText,'String','Select Motor Parameters!');
+    set(handles.MotorParamSelect,'Visible',1);
+else
+    set(handles.L_Get_Setpoint,'String','Get Setpoint');
+    set(handles.L_Set_Setpoint,'String','Set Setpoint');
+    set(handles.Torque_Panel,'Title','Torque');
+    set(handles.CurrentDiagnosticsCheckbox,'Enable','on');
+    set(handles.MotorParamSelect,'Visible',0);
+    set(findall(handles.MotorParamSelect,'-property','value'),'value',0);
+end
+
+% if(bt.Status=="open")
+%     fwrite(bt,char(107));
+% end
 % Hint: get(hObject,'Value') returns toggle state of OLCurrentControl
+
+
+% --- Executes on button press in CurrentDiagnosticsCheckbox.
+function CurrentDiagnosticsCheckbox_Callback(hObject, ~, handles)
+% hObject    handle to CurrentDiagnosticsCheckbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of CurrentDiagnosticsCheckbox
+GUI_Variables = handles.GUI_Variables;
+bt = GUI_Variables.BT;
+
+if get(hObject,'Value')
+    set(handles.L_Get_Setpoint,'String','Get Current');
+    set(handles.L_Set_Setpoint,'String','Set Current');
+    set(handles.Torque_Panel,'Title','Benchtop Current Control Testing');
+    set(handles.OLCurrentControl,'Enable','off');
+    set(handles.statusText,'String','Select Motor Parameters!');
+    set(handles.MotorParamSelect,'Visible',1);
+else
+    set(handles.L_Get_Setpoint,'String','Get Setpoint');
+    set(handles.L_Set_Setpoint,'String','Set Setpoint');
+    set(handles.Torque_Panel,'Title','Torque');
+    set(handles.OLCurrentControl,'Enable','on');
+    set(handles.MotorParamSelect,'Visible',0);
+    set(findall(handles.MotorParamSelect,'-property','value'),'value',0);
+end
+
+% if(bt.Status=="Open")
+%     fwrite(bt,char(108));
+% end
+
